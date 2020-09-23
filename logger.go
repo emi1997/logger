279a296a658bd4c2b71ae4ddbe0ed47bc7820a4a
Julia Kettl <keju@WEDV.Abensberg>
2020-09-23 14:54:36 +0200

    update SetOutputFile

--- logger.go
+++ logger.go
@@ -3,30 +3,52 @@ package logger
 import (
 	// "fmt"
 	"os"
-	"time"
+	// "time"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func Fields(name string, msg string, time time.Time) {
+// type StandardField struct {
+// 	Name string
+// 	Msg  string
+// 	Time time.Time
+// 	// "name": name,
+// 	// 	"msg":  msg,
+// 	// 	"time": time,
+// }
 
-	log.SetFormatter(&log.JSONFormatter{})
-	standardFields := log.Fields{
-		"name": name,
-		"msg":  msg,
-		"time": time,
-	}
-	log.WithFields(standardFields)
-}
+// var (
+// 	InfoLogger *log.Logger
+// 	ErrorLogger *log.Logger
+// )
 
 var logging = log.New()
 
-var (
-	InfoLogger  *log.Logger
-	ErrorLogger *log.Logger
-)
+// type StandardField struct{
+// 	Name string
+// 	Msg string
+// 	Time time.Time
+// }
 
-func SetOutputFile(loggingTo string) string {
+// type LogFields struct {
+// 	s *StandardField[]string
+// }
+// var s  *StandardField
+// var requestLogger = log.WithFields(log.Fields{"Name: ": s.Name, "Msg: ": s.Msg, "Time: ": s.Time})
+
+// func Fields(name string, msg string, time time.Time) *log.Fields{
+
+// 	log.SetFormatter(&log.JSONFormatter{})
+// 	standardFields := log.Fields{
+// 		// "Name": s.Name,
+// 		// "Msg": s.Msg,
+// 		// "Time": s.Time,
+// 	}
+// 	log.WithFields(standardFields)
+// 	return &standardFields
+// }
+
+func SetOutputFile(loggingTo string)string {
 	logFile, err := os.OpenFile(loggingTo, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	logging.Out = os.Stdout
 
@@ -38,14 +60,21 @@ func SetOutputFile(loggingTo string) string {
 	return logFile.Name()
 }
 
-func ErrorLog(err error) {
-	if err != nil {
-		log.Error(err)
-	}
-}
+// // InfoLog makes new Info-Level Input in logfile
+// func (s *StandardField) InfoLog(name string, msg string) {
+	
+// 	// log.Println(InfoLoggerName)
+// }
+
+// //ErrorLog makes new Error-Level input in logfile
+// func ErrorLog(err error) {
+// 	if err != nil {
+// 		// log.NewEntry(ErrorLogger).Error()
+// 	}
+// }
 
 func CheckErr(err error) {
 	if err != nil {
-
+		panic(err)
 	}
 }
